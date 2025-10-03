eaa78aacbaae8de8b80360fed358537122c8aeb8
shivareddy889 <shivakumarreddy814@gmail.com>
2025-10-03 06:56:17 +0000

    added

---
+++ type_con.py
@@ -0,0 +1,22 @@
+a = 20
+b = 30.5
+
+
+sum = a+b
+print("sum of 2 values:",sum)
+
+
+c = "2"
+d = 22
+e = int(c)
+f = d+e
+print("sumof:",f)
+print(type(e))
+print(type(a))
+
+s = 25.55
+h = 2
+i = float(h)
+print(s+i)
+print(type(h))
+print(type(i))
